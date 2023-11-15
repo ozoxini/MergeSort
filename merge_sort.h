39915a786b5974d2c995265d21039c7235a29f25
Kacper Popko <kacperozoo@gmail.com>
2023-11-15 01:38:04 +0100

    Utworzenie pliku merge_sort.h

---
+++ merge_sort.h
@@ -0,0 +1,13 @@
+#ifndef MERGE_SORT_H
+#define MERGE_SORT_H
+
+#include <vector>
+
+namespace MergeSort {
+    void Sort(std::vector<int>& array);
+
+private:
+    void Merge(std::vector<int>& array, int left, int middle, int right);
+}
+
+#endif
